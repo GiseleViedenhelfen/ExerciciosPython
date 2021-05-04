f182122eb0df203237a3c6c5627689625bd8acc8
Gisele Viedenhelfen <gisele.viedenhelfen@gmail.com>
2021-05-04 15:01:57 -0300

    Update ex76.py

--- pacote download/lista-exercicios/ex76.py
+++ pacote download/lista-exercicios/ex76.py
@@ -1,6 +1,6 @@
 lista = ('Pão', 5.00, 'Leite', 3.00, 'Frango', 15.00)
 for pos in range(0, len(lista)):
-    if pos % 2 == 0:
+    if pos % 2 == 0:# se a posição é par = é nome
         print(f'{lista[pos]:.<30}', end='')
-    else:
+    else: #se a posição é impar o item é preço
         print(f' R${lista[pos]:>8.2f}')
