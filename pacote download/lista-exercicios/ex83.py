c62304f71cd17e7f186e3ab001b2c353ad188542
Gisele Viedenhelfen <gisele.viedenhelfen@gmail.com>
2021-05-31 23:42:36 -0300

    ex

---
+++ pacote download/lista-exercicios/ex83.py
@@ -0,0 +1,14 @@
+#código pego dos comentários
+expr = str(input("Digite uma expressão:"))
+pilha = 0
+for cont in expr:
+    if cont == "(":
+        pilha += 1
+    if cont == ")":
+        pilha -= 1
+    if pilha < 0:
+        break
+if pilha == 0:
+    print("Sua expressão é valida!!!")
+else:
+    print("Sua expressão é invalida!!!")
---
+++ pacote download/lista-exercicios/ex96.py
@@ -0,0 +1,6 @@
+def area(l, c):
+    a = l * c
+    print(f'A area do terreno {l} x {c} é: {a}')
+
+
+area(float(input('largura: ')), float(input('comprimento: ')))
